33e9a49c4298a3ddc6e46ac41d3aefeda73779d3
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-06-14 13:33:40 +0300

    Добавление первоначальной структуры папок

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,10 @@
+import cn from 'classnames'
+import styles from './App.module.css'
+
+const App = () => {
+  return (
+    <h1 className={cn(styles.header, styles.text)}>Hello!</h1>
+  )
+}
+
+export default App;
