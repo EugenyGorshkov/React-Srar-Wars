5147ef20cdd5fff2fbc904d3671cc34eabf16d54
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-06-14 14:19:41 +0300

    добавление функционала по запросу данных через API

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,9 +1,8 @@
-import cn from 'classnames'
 import styles from './App.module.css'
 
 const App = () => {
   return (
-    <h1 className={cn(styles.header, styles.text)}>Hello!</h1>
+    <h1 className={styles.header}>Hello!</h1>
   )
 }
 
---
+++ src/utils/network.js
@@ -0,0 +1,49 @@
+
+const SWAPI_ROOT = 'https://swapi.dev/api/';
+const SWAPI_PEOPLE = 'people';
+
+export const getApiResource = async (url) => {
+    try {
+        const res = await fetch(url);
+
+        if(!res.ok) {
+            console.error('Could not fetch.', res.status);
+            return false;
+        }
+
+        return await res.json();
+    }
+    catch (error) {
+        console.error('Could not fetch.', error.message);
+        return false;
+    }
+}
+
+// Асинхронная функция
+
+// (async() => {
+//     const body = await getApiResource(SWAPI_ROOT+SWAPI_PEOPLE)
+//     console.log(body)
+// })()
+
+// Вызов функции через промис
+
+// getApiResource(SWAPI_ROOT+SWAPI_PEOPLE)
+//     .then(body => console.log(body))
+
+
+
+
+
+
+
+
+
+
+// export const getApiResource = (url) => {
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(res => console.log(res))
+//         .catch(error => console.log(error.message))
+    
+// }
