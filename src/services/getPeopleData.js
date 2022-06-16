8779c76cfd27541839de2bc8314656ee249c7241
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-06-16 15:13:58 +0300

    добавление алиасов в импорты

--- src/services/getPeopleData.js
+++ src/services/getPeopleData.js
@@ -1,7 +1,7 @@
 import { 
     HTTPS, SWAPI_ROOT, SWAPI_PEOPLE, 
     GUIDE_IMG_EXTENTION, URL_IMG_PERSON 
-} from '../constants/api';
+} from '@constants/api';
 
 
 const getId = (url, category) => {
