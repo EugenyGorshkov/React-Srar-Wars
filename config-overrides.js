a9fe3e350d05b3e1a73dba52be7062f6beade711
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-06-21 14:49:26 +0300

    добавление storybook к проекту

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ const { alias } = require('react-app-rewire-alias')
 module.exports = function override(config, env) {
     alias({
         '@components': 'src/components',
+        '@ui': 'src/components/UI',
         '@constants': 'src/constants',
         '@containers': 'src/containers',
         '@hoc-helpers': 'src/hoc-helpers',
