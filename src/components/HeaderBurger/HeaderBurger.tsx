0093140c7741c1c0148243cb41af3f4bf9b1a729
EugenyGorshkov <gorshkov.zhenya@mail.ru>
2022-08-14 00:00:37 +0300

    add burger menu _adaptive

--- src/components/HeaderBurger/HeaderBurger.tsx
+++ src/components/HeaderBurger/HeaderBurger.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import cn from 'classnames';
 
-import { Favorite } from 'components/Favorite';
-import { useTheme, THEME_LIGHT, THEME_DARK, THEME_NEITRAL} from 'context';
-
+import { THEME_DARK, THEME_LIGHT, THEME_NEITRAL, useTheme } from 'context';
 import imgDroid from './img/droid.svg';
 import imgLightsiber from './img/lightsaber.svg';
 import imgSpaceStaition from './img/space-station.svg';
 
-import styles from './Header.module.scss';
+import styles from './HeaderBurger.module.scss';
 
-export const Header:React.FC = () => {
+const HeaderBurger: React.FC = () => {
+    const [activeBurger, setActiveBurger] = useState(false);
     const [icon, setIcon] = useState(imgDroid);
     const isTheme = useTheme();
 
@@ -24,26 +24,39 @@ export const Header:React.FC = () => {
             }
         }
         
-    }, [isTheme])
+    }, [isTheme]);
+
+    const activeHandler = () => {
+        activeBurger ? setActiveBurger(false) : setActiveBurger(true)
+    }
 
     return (
-        <div className={styles.container}>
-            
-            <Link to='/'>
+        <div className={styles.burger_wrapper}>
+            <div 
+                className={cn(styles.burger__icon, activeBurger ? styles._active : '')}
+                onClick={activeHandler}
+            >
+                <span></span>
+            </div>
+
+            <Link to='/' className={styles.link_logo}>
                 <img className={styles.logo} src={icon} alt="Star Wars" />
             </Link>
+
             
-            <ul className={styles.list__container}>
+            <ul className={cn(styles.burger_body, activeBurger ? styles._active : '')} onClick={activeHandler}>
                 <li><NavLink to='/'>Home</NavLink></li>
                 <li><NavLink to='/people/?page=1'>People</NavLink></li>
                 <li><NavLink to='/search'>Search</NavLink></li>
                 <li><NavLink to='/not-found'>Not found</NavLink></li>
                 <li><NavLink to='/fail'>Fail</NavLink></li>
-                <li className={styles.loginEl}><NavLink to='/login'>Log In</NavLink></li>
+                <li><NavLink to='/login'>Log In</NavLink></li>
             </ul>
-
-            <Favorite />
         </div>
+
     )
 }
 
+
+
+export default HeaderBurger;
